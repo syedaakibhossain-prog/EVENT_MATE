d4caf248e2771f7ac7cb3f60ded4fba7a79df421
syed aakib hossain <syedaakibhossain@gmail.com>
2026-01-08 20:14:27 -0500

    add eventmate-bckend module

---
+++ eventmate-backend/routes/events.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+    getAllEvents,
+    creatEvents
+} from "../controlers/events.controlers.js";
+
+const router = express.Router();
+
+router.get("/", getAllEvents);
+router.post("/", creatEvents);
+
+export default router;
